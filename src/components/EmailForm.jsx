c00a86d0a1eba55f7a6099332fdd7d861460af3e
nilaysrivastava <nilay2103@gmail.com>
2024-05-23 18:57:30 +0530

    bug fixes

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -8,9 +8,9 @@ const EmailForm = () => {
   const heading = "Let's get in touch!".split("");
 
   const form = useRef();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [sending, setSending] = useState(false);
   const [sent, setSent] = useState(false);
   const [error, setError] = useState(false);
@@ -19,126 +19,134 @@ const EmailForm = () => {
     e.preventDefault();
     setSending(true);
 
-    emailjs.sendForm(
-      "service_ovqgs3v",
-      "template_odtq3pn",
-      form.current,
-      templateParams,
-      "qOBmYqpsDoyYylW0x"
-    )
-    .then((response) => {
-      console.log('Email sent successfully!', response);
-      setSent(true);
-      setName('');
-      setEmail('');
-      setMessage('');
-    })
-    .catch((error) => {
-      console.error('Error sending email:', error);
-      setError(true);
-    })
-    .finally(() => {
-      setSending(false);
-    });
-
-    const templateParams={
+    // Define template parameters
+    const templateParams = {
       from_name: name,
       from_email: email,
       to_name: "Nilay Srivastava",
-      message: message
-    }
+      message: message,
+    };
+
+    emailjs
+      .sendForm(
+        "service_ovqgs3v",
+        "template_odtq3pn",
+        form.current,
+        "qOBmYqpsDoyYylW0x",
+        templateParams // Pass templateParams as the fifth argument
+      )
+      .then((response) => {
+        console.log("Email sent successfully!", response);
+        setSent(true);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+        setError(true);
+      })
+      .finally(() => {
+        setSending(false);
+      });
   };
 
   return (
-    <section >
+    <section>
       <motion.div
-        className="flex flex-col items-center justify-center min-h-screen gap-4 mx-5"
+        className="flex flex-col items-center justify-center min-h-screen gap-4 mx-10"
         variants={fadeIn("down", 0.1)}
         initial={"hidden"}
         whileInView={"show"}
         viewport={{ once: true, amount: 0.7 }}
-        
       >
-        <div style={{
-          backgroundColor: "black",
-          backdropFilter: 'blur(5px)',
-          borderRadius: '20px',
-          boxShadow: '0px 12px 32px 0px purple',
-          padding: '1rem',
-          border: '1px solid #8B5CF6',
-        }}>
-        {/*title*/}
-        <div className="flex flex-col gap-2" >
-          <div className="text-3xl font-bold text-center md:text-4xl lg:text-5xl text-purple-500 pb-4 px-4">
-            {heading.map((letter, index) => (
-              <TextSpan key={index}>
-                {letter === " " ? "\u00A0" : letter}
-              </TextSpan>
-            ))}
+        <div
+          style={{
+            backgroundColor: "black",
+            backdropFilter: "blur(5px)" /* Apply a blur effect */,
+            borderRadius: "20px" /* Rounded corners */,
+            boxShadow: "0px 12px 32px 0px purple" /* Soft shadow */,
+            padding: "1rem" /* Add padding as needed */,
+            border: "1px solid #8B5CF6",
+            marginTop: "4rem",
+          }}
+        >
+          {/*title*/}
+          <div className="flex flex-col gap-2">
+            <div className="text-3xl font-bold text-center md:text-4xl lg:text-5xl text-purple-500 pb-4 px-4">
+              {heading.map((letter, index) => (
+                <TextSpan key={index}>
+                  {letter === " " ? "\u00A0" : letter}
+                </TextSpan>
+              ))}
+            </div>
+            <p className="text-sm text-center md:text-md lg:text-lg text-neutral-300 pb-8">
+              Leave your details and a short message below
+            </p>
           </div>
-          <p className="text-sm text-center md:text-md lg:text-lg text-neutral-300 pb-8">
-            Leave your details and a short message below
-          </p>
-        </div>
-        <form ref={form} onSubmit={handleSubmit}>
-          {/*container*/}
-          <div className="w-full mx-auto bg-black rounded-lg shadow-lg p-7 text-purple-500">
-            {/*name and email*/}
-            <div className="flex flex-col md:flex-row md:justify-between md:gap-4">
-              <div className="flex flex-col">
-                <label className="font-medium">Full name</label>
-                <input
-                  type="text"
-                  className="w-full h-12 p-5 mb-2 font-light rounded-lg md:h-14 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500"
-                  placeholder="Enter your name"
-                  required
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  style={{backgroundColor:"#1a1a1a"}}
-                />
+          <form ref={form} onSubmit={handleSubmit}>
+            {/*container*/}
+            <div className="w-full mx-auto bg-black rounded-lg shadow-lg p-7 text-purple-500">
+              {/*name and email*/}
+              <div className="flex flex-col md:flex-row md:justify-between md:gap-4">
+                <div className="flex flex-col">
+                  <label className="font-medium">Full name</label>
+                  <input
+                    type="text"
+                    className="w-full h-12 p-5 mb-2 font-light rounded-lg md:h-14 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500"
+                    placeholder="Enter your name"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    style={{ backgroundColor: "#1a1a1a" }}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-medium">Email</label>
+                  <input
+                    type="email"
+                    className="w-full h-12 p-5 mb-2 font-light rounded-lg md:h-14 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500"
+                    placeholder="Enter your email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    style={{ backgroundColor: "#1a1a1a" }}
+                  />
+                </div>
               </div>
-              <div className="flex flex-col">
-                <label className="font-medium">Email</label>
-                <input
-                  type="email"
-                  className="w-full h-12 p-5 mb-2 font-light rounded-lg md:h-14 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500"
-                  placeholder="Enter your email"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  style={{backgroundColor:"#1a1a1a"}}
-                />
+              <div className="flex flex-col items-center">
+                <div className="w-full">
+                  <label className="font-medium">Message</label>
+                  <textarea
+                    name="message"
+                    className="w-full p-5 mb-3 font-light rounded-lg md:h-48 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500 "
+                    placeholder="What would you like to ask me?"
+                    rows={3}
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    style={{ backgroundColor: "#1a1a1a" }}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  value="Send"
+                  className="w-full py-2 text-sm font-semibold text-black border-b-4 rounded-full bg-purple-500 hover:bg-purple-800 border-purple-800 hover:border-purple-500"
+                  disabled={sending}
+                >
+                  {sending ? "Sending Message..." : "Send Message"}
+                </button>
+                {sent && (
+                  <p className="mt-2 text-green-500">Sent with success!</p>
+                )}
+                {error && (
+                  <p className="mt-2 text-red-500">
+                    Error sending message. Try later!
+                  </p>
+                )}
               </div>
             </div>
-            <div className="flex flex-col items-center">
-              <div className="w-full">
-                <label className="font-medium">Message</label>
-                <textarea
-                  name="message"
-                  className="w-full p-5 mb-3 font-light rounded-lg md:h-48 placeholder:text-sm placeholder:opacity-80 placeholder:italic focus:outline-purple-500 "
-                  placeholder="What would you like to ask me?"
-                  rows={3}
-                  required
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  style={{backgroundColor:"#1a1a1a"}}
-                />
-              </div>
-              <button
-                type="submit"
-                value="Send"
-                className="w-full py-2 text-sm font-semibold text-black border-b-4 rounded-full bg-purple-500 hover:bg-purple-800 border-purple-800 hover:border-purple-500"
-                disabled={sending}
-              >
-                {sending ? "Sending Message..." : "Send Message"}
-              </button>
-              {sent && <p className="mt-2 text-green-500">Sent with success!</p>}
-              {error && (
-                <p className="mt-2 text-red-500">Error sending message. Try later!</p>
-              )}
-            </div>
-          </div>
-        </form>
+          </form>
         </div>
       </motion.div>
     </section>
--- src/components/Multimedia.jsx
+++ src/components/Multimedia.jsx
@@ -6,7 +6,6 @@ import { fadeIn } from "../utils/variants";
 const Multimedia = () => {
   return (
     <section>
-      {/*Card container*/}
       <div className="grid gap-5 lg:grid-cols-3 max-w-[1500px] items-center">
         {works.multimedia.map((project, index) => {
           return (
@@ -17,20 +16,19 @@ const Multimedia = () => {
               whileInView={"show"}
               viewport={{ once: true, amount: "some" }}
             >
-              {/*image and text*/}
               <div className="grid gap-2 p-8 mx-8 duration-200 ease-in-out bg-white shadow-lg lg:mx-0 rounded-2xl lg:hover:-translate-y-3">
-                {/*image*/}
-                <img className="max-w-full rounded-md" src={project.image} />
-                {/*title*/}
-                <div className="text-[20px] md:text-2xl font-bold">
+                <img
+                  className="max-w-full rounded-md"
+                  src={project.image}
+                  alt={`${project.title} Thumbnail`}
+                />
+                <div className="text-[20px] md:text-2xl font-bold flex items-center">
                   <p>{project.title}</p>
                 </div>
-                {/*description*/}
                 <p>{project.description}</p>
-                {/*tech*/}
                 <div className="flex flex-wrap items-center gap-2 text-sm">
                   <span className="font-semibold">Built with:</span>
-                  <span className=" text-v">{project.stack}</span>
+                  <span className="text-v">{project.stack}</span>
                 </div>
               </div>
             </motion.div>
